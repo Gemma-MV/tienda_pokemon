3c68f94c47e42e5ad8339ff3935512c1312d1266
gemmavalenciano <gemmavalenciano83@gmail.com>
2024-12-12 20:47:59 +0100

    Finalizacion de proyecto

--- src/components/DivStore.js
+++ src/components/DivStore.js
@@ -6,16 +6,15 @@ import borrar from "../assets/img/borrar.png"
 
 // Aqui creamos una funcion llamada PokemonStore para manejar la tienda de pokemons
 const PokemonStore = () => {
-    // Aqui le damos con destructirin a los valores usados en el contexto PokemonContext la funcion para actualizar el pokemon seleccionado (setSelectedPokemon), los datos del pokemon seleccionado (pokemonData), la lista de todos los pokemon (allPokemons) y la funcion para actualizar la lista de todos los pokemon (setAllPokemons)
+    // Aqui le damos con destructirin a los valores usados en el contexto PokemonContext la funcion , son los datos que vamos a necesitar para poder ejecutar el codigo de mas abajo
     const {pokemonData, pokemonPrices, cart, addToCart, removeFromCart, calculateTotalPrice } = useContext(PokemonContext);
-    
-    // Aquí llamamos a un hook personalizado llamado useFetch. Este hook se encarga de obtener los datos del Pokémon seleccionado desde una API
-    // useFetch();
 
 return ( 
   <div className="Main"> 
     <img src={carrito} className="carrito"></img>
     <div className='divContainer'>
+      {/* Aqui verificamos si pokemonData existe y si no esta vacio, lo recorremos con map y lo iteramos */}
+      {/* En el key cogemos la iteracion*/}
       {pokemonData && pokemonData.map((pokemon, i) => ( 
         <div key={i} className='divPokemon'> 
           <h2>{pokemon.name.toUpperCase()}</h2> 
@@ -28,21 +27,21 @@ return (
       <div className="Cart"> 
         <h2>Carrito de Compras</h2> 
         <p className='nCart'>{Object.values(cart).reduce((total, quantity) => total + quantity, 0)}</p>
-        <div className='gray'>
+        <div className='Cart-gray'>
           {Object.keys(cart).length === 0 ? ( 
             <p>El carrito está vacío.</p> 
           ) : ( 
             <ul> 
               {Object.keys(cart).map((pokemonName, index) => ( 
-                <li key={index} className='flex'> 
-                  <button className="delete" onClick={() => removeFromCart(pokemonData.find(p => p.name.toLowerCase() === pokemonName.toLowerCase()))}><img src={borrar}></img></button>
+                <li key={index} className='Cart--flex'> 
+                  <button className="Cartis-delete" onClick={() => removeFromCart(pokemonData.find(p => p.name.toLowerCase() === pokemonName.toLowerCase()))}><img src={borrar}></img></button>
                   {cart[pokemonName]} x {pokemonName.toUpperCase()} - ${pokemonPrices[pokemonName.toLowerCase()]} 
                 </li> 
               ))} 
             </ul>
         )}
         {/* Mostrar el precio total del carrito */} 
-        <div className='blue'>
+        <div className='Cart-blue'>
           <h3>Total de Compra:</h3>
           <p>{calculateTotalPrice()} €</p>
         </div>
--- src/components/PokemonContext.js
+++ src/components/PokemonContext.js
@@ -1,25 +1,27 @@
-// Aqui hacemos la importacion de React, del creador de contenido y de useState, el manejador de estado
+// Aqui hacemos la importacion de React, y las funciones especificas de react del creador de contenido, de useState (para manejar y actualizar estados (valores que pueden cambiar) dentro de un componente) y de useEffect (para ejecutar código en ciertos momentos del ciclo de vida de un componente, como después de que se renderiza)
 import React, { createContext, useState, useEffect } from 'react';
 
-// Aqui creamos el contexto con createContext, lo metemos en la constante PokemonContext (el contesto de los pokemon) y lo exportamos
+// Aqui creamos el contexto con createContext, que permite compartir valores entre componentes y lo metemos en la constante PokemonContext (el contesto de los pokemon) y lo exportamos
 export const PokemonContext = createContext();
 
-// Función para generar precios aleatorios 
+// Función para generar precios aleatorios que sean de minimo 10 y maximo 1000
 const generateRandomPrice = () => { 
     const minPrice = 10; // Precio mínimo 
     const maxPrice = 1000; // Precio máximo 
-    return Math.floor(Math.random() * (maxPrice - minPrice + 1)) + minPrice;
+    // Aqui calculamos de manera aleatoria un numero con Math.random y lo multiplicamos por el numero de la resta de maxPrice menos minPrice (990), de esta manera el numero random nunca va a ser mayor de 1000 ni menor de 10 y como Math.random puede generar un numero decimal lo redondeamos con Math.floor
+    return Math.floor(Math.random() * (maxPrice - minPrice));
 };
 
 // Aqui exportamos la funcion PokemonProvider (proveedor de pokemon) que actua como un componente que provee el contexto a sus hijos. El componente PokemonProvider recibe children como una prop. children representa todos los componentes hijos que estarán envueltos por PokemonProvider
 export const PokemonProvider = ({ children }) => {
-    // Aqui creamos una variable de estado pokemonData y una función para actualizarla setPokemonData e inicializamos el estado pokemonData como un array vacío useState([]).
+    // Aqui creamos una variable de estado pokemonData y una función para actualizarla setPokemonData e inicializamos el estado pokemonData como un array vacío useState([]), de esta manera al principio no contendra ningun valor pero sera una variable que se espera que cambie con el tiempo.
     const [pokemonData, setPokemonData] = useState([]);
     // Aqui creamos una variable de estado allPokemons y una función para actualizarla setAllPokemons e inicializamos el estado allPokemons como un array vacío.
     const [allPokemons, setAllPokemons] = useState([]);
     const [pokemonPrices, setPokemonPrices] = useState({});
     const [cart, setCart] = useState([]);
 
+    // Metemos la api dentro de useEffect porque queremos que la pinte de manera asincrona despues de que haya recibido los datos de la api
     useEffect(() => {
         // Esta api contiene 20 pokemon
         fetch(`https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20`)
@@ -29,40 +31,57 @@ export const PokemonProvider = ({ children }) => {
             const pokemons = data.results.map(pokemon => pokemon.name);
             // Y actualizamos la lista de todos los Pokémons con los nombres obtenidos.
             setAllPokemons(pokemons);
-            // Obtener datos para todos los Pokémon 
+            // Aqui recorremos nuevamente el data que contiene el resultado de los pokemon
             const fetches = data.results.map(pokemon => 
+                // Y hacemos una solicitud a la URL específica de cada Pokémon, convertimos la respuesta de cada solicitud en un objeto JSON y lo almacenamos en la const fetches
                 fetch(pokemon.url).then(res => res.json()) 
             ); 
+            // Promise.all espera a que todas las promesas en el array fetches se completen, las ejecuta cuando se han resuelto y llama a la funcion setPokemonData que actualiza los datos de pokemonData
             Promise.all(fetches).then(pokemonData => { 
                 setPokemonData(pokemonData);
+            // Creamos la constante que contendra los datos de los pokemon
             const prices = {}; 
+            // Recorremos los datos de pokemonData
             pokemonData.forEach(pokemon => { 
-              prices[pokemon.name.toLowerCase()] = generateRandomPrice(); 
+                // A la constante prices segun la posicion del nombre del pokemon llamammos a la funcion generateRandomPrice que le da un precio aleatorio
+                prices[pokemon.name.toLowerCase()] = generateRandomPrice(); 
             }); 
+            // Actualizamos el precio del pokemon llamando a la funcion setPokemonPrices
             setPokemonPrices(prices); 
             });
           });
+        // El [] es un array vacio que provoca que el código dentro de useEffect se ejecute solo una vez, cuando el componente se monte por primera vez.  
         }, []);
 
         // Función para agregar Pokémon al carrito 
         const addToCart = (pokemon) => { 
+            // setCart actualiza el estado del carrito empezando por el estado actual del carrito prevCart
             setCart(prevCart => { 
+                // Creamos una nueva copia del carrito actual guardandola en newCart utilizando el operador de propagación (...) para copiar todas las propiedades del objeto prevCart en un nuevo objeto newCart 
                 const newCart = { ...prevCart }; 
+                // Aqui comprobamos si ya hay un pokemon en el carrito que se mete con la llamada a cart en DivStore dentro del div Cart
                 if (newCart[pokemon.name.toLowerCase()]) { 
+                    // Si hay un pokemon incrementamos su cantidad en 1 cuando volvemos a seleccionar otro
                     newCart[pokemon.name.toLowerCase()] += 1; 
                 } else { 
+                    // Sino deja la cantidad de pokemon en 1
                     newCart[pokemon.name.toLowerCase()] = 1; 
-                } return newCart;
+                } 
+                return newCart;
             });
         };
 
-        // Función para eliminar Pokémon del carrito 
+        // Función para eliminar Pokémon del carrito, creamos la constante que contiene la funcion y actualizamos el carrito con setCart con el estado actual del carrito prevCart
         const removeFromCart = (pokemon) => { 
             setCart(prevCart => { 
+                // Creamos la copia del carrito actual
                 const newCart = { ...prevCart }; 
+                // Si hay un pokemon en el carrito le quitamos 1
                 if (newCart[pokemon.name.toLowerCase()]) { 
                     newCart[pokemon.name.toLowerCase()] -= 1; 
+                    // Verifica si la cantidad de pokemon en el carrito ha llegado a 0
                     if (newCart[pokemon.name.toLowerCase()] === 0) { 
+                        // Si la cantidad del Pokémon es 0, lo elimina del carrito
                         delete newCart[pokemon.name.toLowerCase()]; 
                     } 
                 } 
@@ -72,14 +91,17 @@ export const PokemonProvider = ({ children }) => {
 
         // Función para calcular el precio total del carrito 
         const calculateTotalPrice = () => { 
+            // Object.keys es una función que devuelve un array con las claves del objeto cart que contiene los pokemon añadidos al carrito en DivStore. En este caso, devuelve un array con los nombres de los Pokémons en el carrito los reducimos con reduce a un solo valor, total es el acumulador que mantiene la suma total mientras se recorre el array y pokemonName es el nombre del Pokémon actual que se está procesando en el array
             return Object.keys(cart).reduce((total, pokemonName) => { 
+                // La función reduce devuelve esta expresión en cada iteración, actualizando el acumulador total. pokemonPrices[pokemonName]: accede al precio del Pokémon usando su nombre como clave en el objeto pokemonPrices y cart[pokemonName]: accede a la cantidad del Pokémon en el carrito usando su nombre como clave en el objeto cart. Total suma el precio del pokemon pokemonPrices[pokemonName] por la cantidad de pokemon que hay cart[pokemonName]
                 return total + (pokemonPrices[pokemonName] * cart[pokemonName]); 
+            // 0 es el valor inicial del acumulador del carrito
             }, 0);
         }
         
     // Comienzamos la estructura de retorno del componente.
     return (
-        // Aqui pasamos un objeto con los valores y funciones de actualización del estado para que estén disponibles en el contexto. El proveedor de contexto de pokemon (PokemonContext.Provider) tiene como valores las variables selectedPokemon (seleccionar pokemon), pokemonData y allPokemons y las funciones de devolucion de datos setSelectedPokemon, setPokemonData y setAllPokemons. Y dentro le pasamos los componentes hijos que estan envueltos dentro del creador de contextos
+        // Aqui pasamos un objeto con los valores y funciones de actualización del estado para que estén disponibles en el contexto
         <PokemonContext.Provider value={{ pokemonData, allPokemons,  pokemonPrices, cart, addToCart, removeFromCart, calculateTotalPrice }}>
             {children}
         </PokemonContext.Provider>
